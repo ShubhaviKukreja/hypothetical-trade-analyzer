72ae8f143c66cf6aab5d7becc7a80404c341e12e
Mahekjain2706 <lci2021028@iiitl.ac.in>
2024-03-18 14:23:18 +0530

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import Login from '../src/Components/Login/login.jsx'
 import SignUp from '../src/Components/SignUp/SignUp.jsx'
 // import StockList from './Components/MainPage/mainpage.jsx'
@@ -11,16 +10,6 @@ import Profile from '../src/Components/Profile/profile.jsx'
 import NavBar from '../src/Components/NavBar/navbar.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Main  from '../src/Components/ParticularStock/stock.js';
-=======
-import Login from './Components/Login/login';
-import SignUp from './Components/SignUp/SignUp.jsx'
-import StockList from './Components/MainPage/mainpage.jsx'
-import Main from './Components/ParticularStock/particularStock.js'
-import{
-  BrowserRouter as Router , Routes,Route
-} from 'react-router-dom'
-// import Profile from './Components/Profile/profile.jsx'
->>>>>>> origin/main
 
 function App() {
   return (
@@ -29,18 +18,11 @@ function App() {
       <Router>
         <Routes>
           {/* <Route path='/' element={<Profile/>} /> */}
-<<<<<<< HEAD
           {/* <Route path='/getlist' element={<StockList/>} /> */}
           <Route path='/' element={<Main/>} />
           {/* <Route path='/login' element={<Login/>} /> */}
           {/* <Route path='/signup' element={<SignUp />} /> */}
-=======
-          <Route path='/getlist' element={<StockList/>} />
-          <Route path='/st1' element={<Main/>} />
-          <Route path='/login' element={<Login/>} />
-          <Route path='/signup' element={<SignUp />} />
-          
->>>>>>> origin/main
+
         </Routes>
       </Router>
       <Login/>
